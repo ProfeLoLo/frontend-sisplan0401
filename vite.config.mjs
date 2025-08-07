4053b1b3a898650df160ac2afb98fdfe64b92664
Profe LoLo <amanciololo@gmail.com>
2025-08-07 16:42:46 -0400

    frontend

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,16 +5,23 @@ import react from '@vitejs/plugin-react'
 // Configuración principal de Vite para el sistema Sisplan0401WEB
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
-    port: 5173,      // Puerto local para el frontend
-    open: true,      // Abre el navegador automáticamente
-    proxy: {
-      '/api': 'http://localhost:3000' // Redirige llamadas al backend local
+    //port: 5173,      // Puerto local para el frontend
+    //open: true,      // Abre el navegador automáticamente
+   // proxy: {
+    //  '/api': 'http://localhost:3000' // Redirige llamadas al backend local
+
+        proxy: {
+      '/api': 'https://backend-poa.up.railway.app'
     }
+    //}
   },
   build: {
     outDir: 'dist',  // Carpeta de salida para producción
     sourcemap: true  // Útil para depurar errores en producción (opcional)
   },
-  base: './'         // Usa rutas relativas para despliegue (Netlify, GitHub Pages)
+ // base: './'         // Usa rutas relativas para despliegue (Netlify, GitHub Pages)
 })
+
+
